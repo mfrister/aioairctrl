4b6017917e2975546d9564f3bd522e329ac65a1b
Michael Frister <michael+git@frister.net>
2022-03-18 09:07:01 +0100

    Dependencies: Improve version specifications
    aiocoap: A very specific version makes compatibility with other
             libraries more work (e.g. pytradfri). Allow new patch-level
             versions.
    pycryptodomex: Pin the major version to prevent accidental breakage.

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
     license="MIT",
     packages=find_packages(),
     install_requires=[
-        "aiocoap==0.4.1",
-        "pycryptodomex",
+        "aiocoap>=0.4.1, <0.5",
+        "pycryptodomex>=3.13, <4.0",
     ],
     entry_points={
         "console_scripts": [
