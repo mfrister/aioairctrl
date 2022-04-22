aa202e05a0c51020b5c4593256f5a8751a098f02
Michael Frister <michael+git@frister.net>
2022-04-22 21:07:15 +0200

    Fix package build and pin versions in requirements
    We want to define version ranges for the package to allow for
    easier compatibility with other packages using the same dependencies.
    
    For the CI build, we want to use the exact versions, so the build never
    unexpectedly breaks.

--- setup.py
+++ setup.py
@@ -23,7 +23,10 @@ setup(
     ],
     packages=find_packages(),
     package_data={"phipsair": ["py.typed"]},
-    install_requires=[val.strip() for val in open("requirements.txt")],
+    install_requires=[
+        "aiocoap>=0.4.1, <0.5",
+        "pycryptodomex>=3.13, <4.0",
+    ],
     entry_points={
         "console_scripts": [
             "phipsair=phipsair.__main__:main",
